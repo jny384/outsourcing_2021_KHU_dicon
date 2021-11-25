9b2b9fb25523eb0c37db2b69a59d794258a0cdf1
jny384 <66296986+jny384@users.noreply.github.com>
2021-11-26 07:41:59 +0900

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Header from "./views/home/header";
-import Footer from "./views/home/footer";
+// import Footer from "./views/home/footer";
 import HomeView from "./views/home/HomeView";
 
 import GuestBook from "./views/guest/guestBook";
@@ -51,7 +51,6 @@ function App() {
                         <Route exact path='/works/:teamName' component={WorksList}/>
                     </BrowserRouter>
                 </div>
-                {/*<Footer/>*/}
             </div>
   );
 }
